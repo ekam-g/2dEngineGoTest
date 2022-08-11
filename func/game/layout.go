b888630bec5ff64faf80a380e1608b4da4651f58
Carghai74 <94583299+carghai@users.noreply.github.com>
2022-08-10 23:50:18 -0400

    refoctor

---
+++ func/game/layout.go
@@ -0,0 +1,24 @@
+package game
+
+import "github.com/hajimehoshi/ebiten/v2"
+
+type Game struct{}
+
+// Update proceeds the game state.
+// Update is called every tick (1/60 [s] by default).
+func (g *Game) Update() error {
+	// Write your game's logical update.
+	return nil
+}
+
+// Draw draws the game screen.
+// Draw is called every frame (typically 1/60[s] for 60Hz display).
+func (g *Game) Draw(screen *ebiten.Image) {
+	// Write your game's rendering.
+}
+
+// Layout takes the outside size (e.g., the window size) and returns the (logical) screen size.
+// If you don't have to adjust the screen size with the outside size, just return a fixed size.
+func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
+	return 320, 240
+}
--- main.go
+++ main.go
@@ -1,39 +1,19 @@
 package main
 
 import (
+	"GoGame/func/game"
 	"github.com/hajimehoshi/ebiten/v2"
 	"log"
 )
 
 // Game implements ebiten.Game interface.
-type Game struct{}
-
-// Update proceeds the game state.
-// Update is called every tick (1/60 [s] by default).
-func (g *Game) Update() error {
-	// Write your game's logical update.
-	return nil
-}
-
-// Draw draws the game screen.
-// Draw is called every frame (typically 1/60[s] for 60Hz display).
-func (g *Game) Draw(screen *ebiten.Image) {
-	// Write your game's rendering.
-}
-
-// Layout takes the outside size (e.g., the window size) and returns the (logical) screen size.
-// If you don't have to adjust the screen size with the outside size, just return a fixed size.
-func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
-	return 320, 240
-}
-
 func main() {
-	game := &Game{}
+	NewGame := &game.Game{}
 	// Specify the window size as you like. Here, a doubled size is specified.
 	ebiten.SetWindowSize(640, 480)
 	ebiten.SetWindowTitle("Go Game Example")
-	// Call ebiten.RunGame to start your game loop.
-	if err := ebiten.RunGame(game); err != nil {
+	// Call ebiten.RunGame to start your NewGame loop.
+	if err := ebiten.RunGame(NewGame); err != nil {
 		log.Fatal(err)
 	}
 }
