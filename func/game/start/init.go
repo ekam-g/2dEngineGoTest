95493708960e979e17d8d0fcb2a2bfd345175289
Carghai74 <94583299+carghai@users.noreply.github.com>
2022-08-11 23:21:24 -0400

    WORKED!!!!!!!!!

--- func/game/start/init.go
+++ func/game/start/init.go
@@ -4,15 +4,21 @@ import (
 	"errors"
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+	_ "image/jpeg"
 )
 
 type Init struct {
 	mainSprite *ebiten.Image
+	fullFace   *ebiten.Image
 }
 
 func (Init) Start() (err error, r Init) {
 	// Write your start cod here.
-	r.mainSprite, _, err = ebitenutil.NewImageFromFile("assets/mainGo.png")
+	r.fullFace, _, err = ebitenutil.NewImageFromFile("assets/golang_gofer.jpeg")
+	if err != nil {
+		return errors.New("failed when initializing sprite, error details:\n\n" + err.Error()), r
+	}
+	r.mainSprite, _, err = ebitenutil.NewImageFromFile("assets/golang_gofer.jpeg")
 	if err != nil {
 		return errors.New("failed when initializing sprite, error details:\n\n" + err.Error()), r
 	}
