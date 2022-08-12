cfd71c0ba892f70c109dc447da8147c946292513
Carghai74 <94583299+carghai@users.noreply.github.com>
2022-08-12 16:51:21 -0400

    worked!

--- func/game/layout.go
+++ func/game/layout.go
@@ -1,8 +1,15 @@
 package game
 
 import (
+	"errors"
 	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+	_ "image/jpeg"
+	_ "image/png"
 )
+
+var fullFace *ebiten.Image
+var mainSprite *ebiten.Image
 
 type Game struct{}
 
@@ -16,11 +23,24 @@ func (g *Game) Update() error {
 // Draw draws the game screen.
 // Draw is called every frame (typically 1/60[s] for 60Hz display).
 func (g *Game) Draw(screen *ebiten.Image) {
-
+	screen.DrawImage(mainSprite, nil)
 }
 
 // Layout takes the outside size (e.g., the window size) and returns the (logical) screen size.
 // If you don't have to adjust the screen size with the outside size, just return a fixed size.
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
-	return 320, 240
+	return 1600, 900
+}
+
+func (Game) Start() (err error) {
+	// Write your start cod here.
+	fullFace, _, err = ebitenutil.NewImageFromFile("assets/golang_gofer.jpeg")
+	if err != nil {
+		return errors.New("failed when initializing sprite, error details:\n\n" + err.Error())
+	}
+	mainSprite, _, err = ebitenutil.NewImageFromFile("assets/mainGo.png")
+	if err != nil {
+		return errors.New("failed when initializing sprite, error details:\n\n" + err.Error())
+	}
+	return nil
 }
--- main.go
+++ main.go
@@ -2,23 +2,22 @@ package main
 
 import (
 	"GoGame/func/game"
-	"GoGame/func/game/start"
 	"github.com/hajimehoshi/ebiten/v2"
 	"log"
 )
 
 // Game implements ebiten.Game interface.
 const (
-	screenWidth  = 800
-	screenHeight = 600
+	screenWidth  = 1600
+	screenHeight = 900
 )
 
 func main() {
-	err, _ := start.Init{}.Start()
-	if err != nil {
-		log.Println(err)
-	}
 	NewGame := &game.Game{}
+	err := NewGame.Start()
+	if err != nil {
+		log.Panic(err)
+	}
 	ebiten.SetWindowSize(screenWidth, screenHeight)
 	ebiten.SetWindowTitle("Go Game Example")
 	if err := ebiten.RunGame(NewGame); err != nil {
